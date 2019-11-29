1a62c0e8239cd1f95c6b7ec813928ca968528e32
carmelogalia <p.militante@creativehope.jp>
2019-11-29 18:52:39 +0900

    start react forms part 1

--- src/App.js
+++ src/App.js
@@ -15,46 +15,23 @@ import productsData from './components/vschoolProducts'
 import TodoItem from "./components/TodoItem"
 import todosData from "./components/todosData"
 
+import Conditional from './components/Conditional'
+
+//https://scrimba.com/p/p7P5Hd/cW8Jdfy
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {
-      todos: todosData
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(id) {
-    // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-    // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-    this.setState(prevState => {
-      const newTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed
-        }
-        return todo
-      })
-      return {
-        todos: newTodos
-      }
-    })
+    this.state = {}
   }
 
   render() {
-    const todoItems = this.state.todos.map(task =>
-      <TodoItem
-        key={task.id}
-        task={task}
-        handleChange={this.handleChange}
-      />)
-
     return (
-      <div className="todo-list">
-        {todoItems}
-      </div>
+      <div>
+        Code goes here
+        </div>
     )
   }
 }
+
 
 export default App
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,22 @@
 import React from "react"
+import { cpus } from "os"
 
 function TodoItem(props) {
+
+    const completedStyle = {
+        color: "#cdcdcd",
+        fontStyle: "italic",
+        textDecoration: "line-through"
+    }
+
     return (
         <div className="todo-item">
             <input
                 type="checkbox"
-                checked={props.task.completed}
-                onChange={() => props.handleChange(props.task.id)}
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
             />
-            <p>{props.task.text}</p>
+            <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
         </div>
     )
 }
