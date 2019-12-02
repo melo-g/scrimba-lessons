2ed48d381a2358598fc6451287aa602cf714dcfc
carmelogalia <c.galia@creativehope.co.jp>
2019-12-02 18:36:11 +0900

    react form practice

--- src/App.js
+++ src/App.js
@@ -17,21 +17,132 @@ import todosData from "./components/todosData"
 
 import Conditional from './components/Conditional'
 
-//https://scrimba.com/p/p7P5Hd/cW8Jdfy
-class App extends React.Component {
+//https://scrimba.com/p/p7P5Hd/ceLWEsp
+class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      firstName: "",
+      lastName: "",
+      age: "",
+      gender: "",
+      location: "",
+      light: false,
+      medium: false,
+      heavy: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const { name, value, checked, type } = event.target
+    type === "checked" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
   }
 
   render() {
+    const { light, medium, heavy } = this.state
+    console.log(light, medium, heavy)
     return (
-      <div>
-        Code goes here
-        </div>
+      <main>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            placeholder="First Name"
+          />
+          <br />
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+            placeholder="Last Name"
+          />
+          <br />
+          <input
+            type="text"
+            name="age"
+            value={this.state.age}
+            onChange={this.handleChange}
+            placeholder="Age"
+          />
+          <br />
+
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              onChange={this.handleChange}
+            /> Male
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              onChange={this.handleChange}
+            /> Female
+          </label>
+          <br />
+
+          <select
+            type="select"
+            name="location"
+            onChange={this.handleChange}
+          >
+            <option value="">---Please select a destination---</option>
+            <option value="coron">Coron</option>
+            <option value="laguna">Laguna</option>
+            <option value="el nido">El Nido</option>
+          </select>
+          <br />
+
+          <label>
+            <input
+              type="checkbox"
+              name="light"
+              onChange={this.handleChange}
+              checked={this.state.light}
+            />Light
+          </label>
+
+          <label>
+            <input
+              type="checkbox"
+              name="medium"
+              onChange={this.handleChange}
+              checked={this.state.medium}
+            />Medium
+          </label>
+
+          <label>
+            <input
+              type="checkbox"
+              name="heavy"
+              onChange={this.handleChange}
+              checked={this.state.heavy}
+            />Heavy
+          </label>
+          <br />
+
+          <button>Submit</button>
+        </form>
+        <hr />
+        <h2>Entered information:</h2>
+        <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+        <p>Your age: {this.state.age}</p>
+        <p>Your gender: {this.state.gender}</p>
+        <p>Your destination: {this.state.location}</p>
+        <p>
+          Your dietary restrictions:
+          Light: {this.state.light}
+        </p>
+      </main>
     )
   }
 }
-
-
 export default App
