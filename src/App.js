0d47534af11806828dd6742a76e97fa84f283e60
carmelogalia <c.galia@creativehope.co.jp>
2019-12-03 08:57:28 +0800

    test

--- src/App.js
+++ src/App.js
@@ -21,125 +21,37 @@ import Conditional from './components/Conditional'
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      firstName: "",
-      lastName: "",
-      age: "",
-      gender: "",
-      location: "",
-      light: false,
-      medium: false,
-      heavy: false
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(event) {
-    const { name, value, checked, type } = event.target
-    type === "checked" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    this.state = {}
   }
 
   render() {
-    const { light, medium, heavy } = this.state
-    console.log(light, medium, heavy)
     return (
       <main>
         <form>
-          <input
-            type="text"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleChange}
-            placeholder="First Name"
-          />
-          <br />
-          <input
-            type="text"
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleChange}
-            placeholder="Last Name"
-          />
-          <br />
-          <input
-            type="text"
-            name="age"
-            value={this.state.age}
-            onChange={this.handleChange}
-            placeholder="Age"
-          />
+          <input placeholder="First Name" /><br />
+          <input placeholder="Last Name" /><br />
+          <input placeholder="Age" /><br />
+
+          {/* Create radio buttons for gender here */}
           <br />
 
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-              onChange={this.handleChange}
-            /> Male
-          </label>
-
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="female"
-              onChange={this.handleChange}
-            /> Female
-          </label>
+          {/* Create select box for location here */}
           <br />
 
-          <select
-            type="select"
-            name="location"
-            onChange={this.handleChange}
-          >
-            <option value="">---Please select a destination---</option>
-            <option value="coron">Coron</option>
-            <option value="laguna">Laguna</option>
-            <option value="el nido">El Nido</option>
-          </select>
-          <br />
-
-          <label>
-            <input
-              type="checkbox"
-              name="light"
-              onChange={this.handleChange}
-              checked={this.state.light}
-            />Light
-          </label>
-
-          <label>
-            <input
-              type="checkbox"
-              name="medium"
-              onChange={this.handleChange}
-              checked={this.state.medium}
-            />Medium
-          </label>
-
-          <label>
-            <input
-              type="checkbox"
-              name="heavy"
-              onChange={this.handleChange}
-              checked={this.state.heavy}
-            />Heavy
-          </label>
+          {/* Create check boxes for dietary restrictions here */}
           <br />
 
           <button>Submit</button>
         </form>
         <hr />
         <h2>Entered information:</h2>
-        <p>Your name: {this.state.firstName} {this.state.lastName}</p>
-        <p>Your age: {this.state.age}</p>
-        <p>Your gender: {this.state.gender}</p>
-        <p>Your destination: {this.state.location}</p>
+        <p>Your name: {/* First and last name here */}</p>
+        <p>Your age: {/* Age here */}</p>
+        <p>Your gender: {/* Gender here */}</p>
+        <p>Your destination: {/* Destination here */}</p>
         <p>
           Your dietary restrictions:
-          Light: {this.state.light}
+                  {/* Dietary restrictions here, comma separated */}
         </p>
       </main>
     )
